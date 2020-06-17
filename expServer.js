433782e811d1feac9ea1431f1808d3f6aae3d676
RadhikaSonawane <41570546+RadhikaSonawane@users.noreply.github.com>
2020-06-17 11:14:47 +0200

    Sum calculator

--- expServer.js
+++ expServer.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
     res.send("Welcome to my server");
@@ -16,6 +19,16 @@ app.get("/contactUs", function(req, res){
 
 app.get("/calculator", function(req, res){
     res.sendFile(__dirname + "/index.html");
+})
+
+
+app.post("/calculator", function(req, res){
+    console.log(req.body);
+    let n1 = Number(req.body.v1);
+    let n2 = Number(req.body.v2);
+    
+    let sum = n1 + n2;
+    res.send("Answer : " +sum);
 })
 
 app.listen(5000, function(req, res){
